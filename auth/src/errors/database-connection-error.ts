2d8e24d857693af853209dd4cbeb0d3fe8140211
rolandpakai <roland.pakai@liferay.com>
2022-12-20 08:26:52 +0100

    Response Normalization Strategies

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,19 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connection to database';
+  statusCode = 500;
+  
+  constructor() {
+    super('Error connection to database');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason }
+    ]
+  }
+}
---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,26 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+/*interface CustomError {
+  statusCode: number;
+  serializeErrors(): {
+    message: string;
+    field?: string;
+  }[];
+}*/
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super('Invalid request parameters');
+
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(error => {
+      return { message: error.msg, field: error.param}
+    });
+  }
+}
